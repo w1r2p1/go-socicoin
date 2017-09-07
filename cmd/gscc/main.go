0032d809253253a795607aca23a8ed4fdff1c51a
yangxiaogang <yxg.ok@hotmail.com>
2017-09-07 11:26:43 +0800

    add socicoin code example

---
+++ cmd/gscc/main.go
@@ -0,0 +1,14 @@
+// Copyright 2017 The go-socicoin Authors
+// This file is part of go-socicoin.
+//
+// 
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    fmt.Println("go-socicoin starting...");
+};
+
